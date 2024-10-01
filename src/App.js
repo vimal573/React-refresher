db895693715ef1b82f57baf15c22c5769e880538
Vimal Kumar <Vimal2540.vk@gmail.com>
2024-10-01 13:58:58 +0530

    Parent Child Comunication

--- src/App.js
+++ src/App.js
@@ -6,15 +6,20 @@ import './App.css';
 
 const App = () => {
   const courseGoals = [
-    {id: 'cg1', text: 'Finish the Course'},
-    {id: 'cg2', text: 'Learn all about the Course Main Topic'},
-    {id: 'cg3', text: 'Help other students in the Course Q&A'},
+    { id: 'cg1', text: 'Finish the Course' },
+    { id: 'cg2', text: 'Learn all about the Course Main Topic' },
+    { id: 'cg3', text: 'Help other students in the Course Q&A' }
   ];
+
+  const addNewGoalHandler = newGoal => {
+    courseGoals.push(newGoal);
+    console.log(courseGoals);
+  };
 
   return (
     <div className="course-goals">
       <h2>Course Goals</h2>
-      <NewGoal />
+      <NewGoal onAddGoal={addNewGoalHandler} />
       <GoalList goals={courseGoals} />
     </div>
   );
--- src/components/NewGoal/NewGoal.js
+++ src/components/NewGoal/NewGoal.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 import './NewGoal.css';
 
-const NewGoal = () => {
+const NewGoal = props => {
   const addGoalHandler = event => {
     event.preventDefault();
 
@@ -11,7 +11,7 @@ const NewGoal = () => {
       text: 'My new goal!'
     };
 
-    console.log(newGoal);
+    props.onAddGoal(newGoal);
   };
 
   return (
