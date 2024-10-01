05b1fa1827bc6171c746a792a303cfe7ad564e21
Vimal Kumar <Vimal2540.vk@gmail.com>
2024-10-01 13:53:51 +0530

    React Basics

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const App = () => {
-  return <h1>A React App!</h1>;
+  return <h1 title="This works!">Hi, <span>this</span> is ReactJS!</h1>;
 };
 
 export default App;
