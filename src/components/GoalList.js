e8ce0938b361b8e2b333c81106a26da59d7e6deb
Vimal Kumar <Vimal2540.vk@gmail.com>
2024-10-01 13:56:18 +0530

    Passing Props

--- src/components/GoalList.js
+++ src/components/GoalList.js
@@ -2,12 +2,11 @@ import React from 'react';
 
 import './GoalList.css';
 
-const GoalList = () => {
+const GoalList = props => {
+  console.log(props.goals);
   return (
     <ul className="goal-list">
-      <li>Finish the Course</li>
-      <li>Learn all about the Course Main Topic</li>
-      <li>Help other students in the Course Q&amp;A</li>
+      
     </ul>
   );
 };
